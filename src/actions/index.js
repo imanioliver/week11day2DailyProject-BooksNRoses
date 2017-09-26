af3fc4031d8c4a66c2a794aac0c4e88525bd5188
Imani Oliver <isaac@ihardy.co>
2017-09-26 14:19:00 -0400

    renders to screen with details

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 
 //select book is an action creator, it will need an action type
 export function selectBook(book) {
-
   return {
+      type:"BOOK_SELECTED",
+      payload: book,
     //You should create a Type and Payload for your Action here
   };
 }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -8,22 +8,19 @@ import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   render() {
-
+      console.log("props: ", this.props.books);
     //must create a map function here to return the following:
 
 
-    // ******************
-    //
-    //     <li
-    //       key={book.id}
-    //       onClick={() => this.props.selectBook(book)}
-    //       className="list-group-item">{book.title}</li>
-    //
-    // ******************
+
+    let listOfBooks = this.props.books.map((book)=>{
+
+        return(<li key={book.id} onClick={() => this.props.selectBook(book)} className="list-group-item">{book.title}</li>)
+    })
 
     return (
       <ul className="list-group col-sm-4">
-        {/* return your mapped array list items here */}
+        {listOfBooks}
       </ul>
     );
   }
